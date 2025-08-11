c8e22e057d5e1acab0d3bfd5770d4527de2aec84
izzy2541 <isabel.donnelly@hotmail.com>
2025-08-11 19:27:07 +1000

    Local query scopes

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -13,5 +14,12 @@ class Book extends Model
     {
         //basically says that a book can have many reviews
         return $this->hasMany(Review::class);
+    }
+
+    //query scopes always have to be prefixed with "scope", then
+    //can be followed by anything
+    public function scopeTitle(Builder $query, string $title): Builder
+    {
+        return $query->where('title', 'LIKE', '%' . $title . '%');
     }
 }
