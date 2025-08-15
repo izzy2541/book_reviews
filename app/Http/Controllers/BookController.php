a45b372b0e3763bd903c02f6f9982c033f2c563f
izzy2541 <isabel.donnelly@hotmail.com>
2025-08-15 12:00:44 +1000

    Cache and caching queries

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Book;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class BookController extends Controller
 {
@@ -29,7 +30,15 @@ class BookController extends Controller
             default => $books->latest()
         };
 
-        $books = $books->get();
+        //arguments are key, how long you want to store the data in seconds, function that will return the actual data
+        // $books = $books->get();
+
+        // with caching you always need a key, so we need to tell the
+        //caching mechanism which key we want to store data under
+        //remember will see whether the file or redis server already contains the key,
+        //if not, it will run function in 3rd argument.
+        $cacheKey = 'books:' . $filter . ':' . $title;
+        $books = Cache::remember($cacheKey, 3600, fn() => $books->get());
 
         return view('books.index', ['books' => $books]);
     }
